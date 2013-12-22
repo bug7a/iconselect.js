6c4ae6bf6233d1c2e7bec87100ce4c72a4fa6a3d
bugra ozden <bugra.ozden@gmail.com>
2013-12-22 09:20:23 +0200

    publish

---
+++ js/analytic.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: bugraozden
+ * Date: 8/3/13
+ * Time: 1:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-5518534-3']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
